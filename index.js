6bc3e100cccd9522a7b8c6381301430218f9b9aa
jimlemoine <jlemoine007@gmail.com>
2021-10-28 09:56:49 -0700

    work in progress

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.send(
+        `<h1>Web 47 rocks!</h1>`
+    )
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
+
+function web47rocks() {
+    console.log(`web ${process.argv[2]} rocks`)
+}
+web47rocks()
+
+function cohortRocksEnv() {
+    console.log(`home folder is ${process.env.HOME} and FOO is ${process.env.FOO}`)
+}
+cohortRocksEnv()
